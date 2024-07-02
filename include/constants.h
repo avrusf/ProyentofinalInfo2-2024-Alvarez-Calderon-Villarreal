49d1addc901d56468a440c0a77d15617bea01d1d
Santino Calderon <contacto.santinocalderon@gmail.com>
2024-07-02 20:34:49 -0300

    Test main

--- include/constants.h
+++ include/constants.h
@@ -1,2 +1,4 @@
 #define LED_PIN 13
 #define SENSOR_PIN 13
+
+#define LED_TEST_PIN 2
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
 #include "../include/constants.h"
 
-int main()
+void setup()
 {
-    LED_PIN;
-    return 0;
+    // Inicializar el pin del LED como salida
+    pinMode(LED_TEST_PIN, OUTPUT);
+}
+
+void loop()
+{
+    // Encender el LED
+    digitalWrite(LED_TEST_PIN, HIGH);
+    delay(1000); // Esperar 1 segundo
+
+    // Apagar el LED
+    digitalWrite(LED_TEST_PIN, LOW);
+    delay(1000); // Esperar 1 segundo
 }
