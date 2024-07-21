14881a3018f6cd8e7c2e83bf08ebb39972158940
Santino Calderon <contacto.santinocalderon@gmail.com>
2024-07-21 14:53:15 -0300

    Clase Fingerprint

---
+++ include/Pins.h
@@ -0,0 +1,7 @@
+#define LED_PIN 13
+#define SENSOR_PIN 13
+
+#define LED_TEST_PIN 2
+
+#define SERIAL_RX_PIN 4
+#define SERIAL_TX_PIN 5
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 #include <Arduino.h>
-#include "../include/constants.h"
+#include "../include/Pins.h"
+
+#include <SoftwareSerial.h>
+#include "../include/Fingerprint.h"
+
+SoftwareSerial mySerial(SERIAL_RX_PIN, SERIAL_TX_PIN); // Crear Serial para Sensor  Rx, TX
+Fingerprint myFingerprint(&mySerial);                  // Crear una instancia de la clase Fingerprint
 
 void setup()
 {
-    // Inicializar el pin del LED como salida
-    pinMode(LED_TEST_PIN, OUTPUT);
+    Serial.begin(9600);
+    myFingerprint.initialize();
 }
 
 void loop()
 {
-    // Encender el LED
-    digitalWrite(LED_TEST_PIN, HIGH);
-    delay(1000); // Esperar 1 segundo
-
-    // Apagar el LED
-    digitalWrite(LED_TEST_PIN, LOW);
-    delay(1000); // Esperar 1 segundo
+    if (myFingerprint.verify_footprint())
+    {
+        Serial.println("Huella válida detectada");
+    }
+    else
+    {
+        Serial.println("Huella no válida o no detectada");
+    }
+    delay(1000); // Espera 1 segundo antes de la siguiente lectura
 }
